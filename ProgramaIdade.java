20acc5953ffaa5f64dc8c427cbf8681e590f3fed
ed05s <edinaldolouredomarques@gmail.com>
2021-12-07 15:34:39 -0300

    corrigi um erro de digitação na linha do else

--- ProgramaIdade.java
+++ ProgramaIdade.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -26,7 +26,7 @@ public class ProgramaIdade {
             System.out.println("maior de idade");       
             
         }else{
-            System.out.println("menor deidade");
+            System.out.println("menor de idade");
         }
         
         
